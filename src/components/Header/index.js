579c94a8cddaf76454aed5a0437e788d04de8ae5
meokisama <hoangdinhsang123@gmail.com>
2024-01-16 14:21:33 +0700

    add bubble effect for header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 import './Header.scss';
 import banner from '../../assets/mignon.jpg'
+import bubble from "../../assets/bubble.png"
 
 function Header() {
 
@@ -31,6 +32,28 @@ function Header() {
         <div className='header'>
             <img id='banner' src={banner} alt='Ranobe Giveaway Booster' />
             <div style={{ height: imgHeight }}></div>
+            <div className="bubbles">
+                <div className="bubble"><img alt='bubble' src={bubble} /></div>
+                <div className="bubble"><img alt='bubble' src={bubble} /></div>
+                <div className="bubble"><img alt='bubble' src={bubble} /></div>
+                <div className="bubble"><img alt='bubble' src={bubble} /></div>
+                <div className="bubble"><img alt='bubble' src={bubble} /></div>
+                <div className="bubble"><img alt='bubble' src={bubble} /></div>
+                <div className="bubble"><img alt='bubble' src={bubble} /></div>
+                <div className="bubble"><img alt='bubble' src={bubble} /></div>
+                <div className="bubble"><img alt='bubble' src={bubble} /></div>
+                <div className="bubble"><img alt='bubble' src={bubble} /></div>
+                <div className="bubble"><img alt='bubble' src={bubble} /></div>
+                <div className="bubble"><img alt='bubble' src={bubble} /></div>
+                <div className="bubble"><img alt='bubble' src={bubble} /></div>
+                <div className="bubble"><img alt='bubble' src={bubble} /></div>
+                <div className="bubble"><img alt='bubble' src={bubble} /></div>
+                <div className="bubble"><img alt='bubble' src={bubble} /></div>
+                <div className="bubble"><img alt='bubble' src={bubble} /></div>
+                <div className="bubble"><img alt='bubble' src={bubble} /></div>
+                <div className="bubble"><img alt='bubble' src={bubble} /></div>
+                <div className="bubble"><img alt='bubble' src={bubble} /></div>
+            </div>
         </div>
     )
 }
