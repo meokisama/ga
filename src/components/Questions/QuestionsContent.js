92dd41799e3fae3753acb135f1340f5cfa71607f
meokisama <hoangdinhsang123@gmail.com>
2024-01-16 09:19:19 +0700

    finish quiz section

---
+++ src/components/Questions/QuestionsContent.js
@@ -0,0 +1,162 @@
+export const questionContent = [
+    {
+        question: "Trong tác phẩm World Teacher, tộc Elf quy định rằng những người trẻ tuổi sau khi trở về từ cuộc hành trình thì họ phải ở lại trong khu rừng bao lâu?",
+        optionA: "1 năm",
+        optionB: "3 năm",
+        optionC: "5 năm",
+        optionD: "10 năm",
+        correctOption: "optionD"
+    },
+    {
+        question: "Trong cuốn thứ 2 “Cô Gái Văn Chương Và Hồn Ma Đói Khát” kể về câu chuyện đầy bi kịch của thiếu nữ Amemiya Hotaru, đoạn thông điệp '1 28 6 4 27 35 3 13 6 4 31 35 2 26 4 35 12 28 29 26 4 35 24 18 4 27 30' được lặp lại 3 lần ở cuối cùng có nghĩa là gì?",
+        optionA: "Em yêu anh",
+        optionB: "Không muốn lên thiên đàng",
+        optionC: "Hãy nhìn em chứ không phải mẹ",
+        optionD: "Tội lỗi cũng mặc kệ, bị trừng phạt cũng được",
+        correctOption: "optionB"
+    },
+    {
+        question: "Mức độ skinship cao nhất của Masachika và Arya trong “Arya bàn bên thỉnh thoảng lại trêu ghẹo tôi bằng tiếng Nga” là gì?",
+        optionA: "Bị đánh",
+        optionB: "Nắm tay",
+        optionC: "Hôn má",
+        optionD: "Sờ ngực trần",
+        correctOption: "optionD"
+    },
+    {
+        question: "Ai là con gái ruột của Nữ hoàng Mylene trong MobuSeka (Thế giới OtomeGame thật khắc nghiệt với nhân vật quần chúng)?",
+        optionA: "Hertrude",
+        optionB: "Erica",
+        optionC: "Jake",
+        optionD: "Finley",
+        correctOption: "optionB"
+    },
+    {
+        question: "Trong Thiên Sứ Nhà Bên, bố mẹ Amane (Shuto và Shihoko) gặp nhau lần đầu tiên khi nào?",
+        optionA: "Bạn thuở nhỏ",
+        optionB: "Ở cấp 3",
+        optionC: "Lên đại học",
+        optionD: "Khi đi làm",
+        correctOption: "optionC"
+    },
+    {
+        question: "Tác phẩm nào sau đây không phải là một tác phẩm của nhà văn Mikawa Ghost?",
+        optionA: "Kết hôn với đứa con gái cùng lớp mà tôi cực ghét (Kurakon)",
+        optionB: "Sống Cùng Em Kế (Gimai Seikatsu)",
+        optionC: "Con em thằng bạn chỉ cà khịa mỗi tôi (Imouza)",
+        optionD: "Anh trai tự xưng hạng F (Jishou F-Rank no Oniisama)",
+        correctOption: "optionA"
+    },
+    {
+        question: "Trong tác phẩm “Sống cùng nữ sinh dễ thương nhất Nhật Bản” (kakao minh hoạ), câu chuyện kết thúc với việc Kaede đi du học, thời gian du học là bao lâu trước khi Kaede trở về cưới Yuuya?",
+        optionA: "2 năm",
+        optionB: "3 năm",
+        optionC: "4 năm",
+        optionD: "5 năm",
+        correctOption: "optionC"
+    },
+    {
+        question: "Tiểu thuyết nào sau đây không có phiên bản web novel được đăng nhiều kỳ trên internet?",
+        optionA: "Sword Art Online",
+        optionB: "KonoSuba",
+        optionC: "Danmachi",
+        optionD: "Goblin Slayer",
+        correctOption: "optionD"
+    },
+    {
+        question: "Trong tác phẩm Thất Nghiệp Chuyển Sinh (Mushoku Tensei), Rudeus đã đặt tên cho tay phải của mình là Hulk, tay trái là Hercules, chân phải là Tindalos, còn chân trái là gì?",
+        optionA: "Belphegor",
+        optionB: "Baskerville",
+        optionC: "Asmodeus",
+        optionD: "Artemis",
+        correctOption: "optionB"
+    },
+    {
+        question: "Trong “Ta là chúa tể độc ác của Đế Quốc liên thiên hà” (Ore wa Seikan Kokka no Akutoku Ryoushu), Liam được dạy Nhất Trảm kiếm pháp từ một ông thầy lừa đảo, vợ của ông thầy này tên là gì?",
+        optionA: "Nina",
+        optionB: "Nias",
+        optionC: "Nino",
+        optionD: "Niichan",
+        correctOption: "optionA"
+    },
+    {
+        question: "Tổng số tiền cần thiết để mua tất cả các tập Light Novel đã được phát hành ở Việt Nam tính đến cuối tháng 2/2023 là bao nhiêu?",
+        optionA: "Từ 56.000.000đ đến 57.000.000đ",
+        optionB: "Từ 61.000.000đ đến 62.000.000đ",
+        optionC: "Từ 70.000.000đ đến 71.000.000đ",
+        optionD: "Từ 88.000.000đ đến 89.000.000đ",
+        correctOption: "optionC"
+    },
+    {
+        question: "Tiểu thuyết nào sau đây chưa được công bố bản quyền ở Việt Nam?",
+        optionA: "Saenai Heroine no Sodatekata",
+        optionB: "Hyakuman Kounen no Chotto Saki",
+        optionC: "Hoshi Furu Yoru ni Nattara",
+        optionD: "Madan no Ou to Vanadis",
+        correctOption: "optionD"
+    },
+    {
+        question: "Tiểu thuyết nào sau đây đã đứng thứ mười hai (#12) trong bảng xếp hạng tổng thể của “Kono Light Novel ga Sugoi! 2023”?",
+        optionA: "Genjitsu de Rabukome Dekinai to Dare ga Kimeta",
+        optionB: "Watashi, Nibanme no Kanojo de Ii kara",
+        optionC: "Wasuresasete yo, Kouhai-kun",
+        optionD: "Mushoku Tensei",
+        correctOption: "optionA"
+    },
+    {
+        question: "Đơn vị nào sau đây chưa tham gia vào phát hành Light Novel ở Việt Nam?",
+        optionA: "Thái Hà Books",
+        optionB: "TA Books",
+        optionC: "Quảng Văn",
+        optionD: "First News",
+        correctOption: "optionD"
+    },
+    {
+        question: "Nhân vật “Tôi” trong bộ tiểu thuyết “Lời Nói Đùa” đã rút khỏi chương trình đào tạo nào trước tập 1?",
+        optionA: "Chương trình STEM+C2",
+        optionB: "Chương trình Rokumeikan",
+        optionC: "Chương trình Ritsumeikan",
+        optionD: "Chương trình ER3",
+        correctOption: "optionD"
+    },
+    {
+        question: "Họa sĩ nào sau đây chưa tham gia minh họa bất kỳ tác phẩm Light Novel nào ở Nhật Bản?",
+        optionA: "Asanagi",
+        optionB: "Takeda Hiromitsu",
+        optionC: "Iida Pochi",
+        optionD: "Shindol",
+        correctOption: "optionD"
+    },
+    {
+        question: "Tên bản thảo nào sau đây KHÔNG xuất hiện trong tác phẩm “Chàng Thẩm Định Viên và Nữ Tác Giả Tập Sự”?",
+        optionA: "Một Thằng Bỏ Học, Mẫu Cảm Lại Đa Tình Tôi Đây, Đi Đến Thế Giới Thứ Ba Liền Kiếm Được Cô Bạn Gái Thứ Ba",
+        optionB: "Tiểu Thuyết Về Căn Phòng Bí Mật",
+        optionC: "Tôi Một Mình Lạc Vào Thế Giới Khác, Là Anh Hùng Cũng Là Quỷ Vương, Vua của Một Harem",
+        optionD: "Tái Sinh Thành Kẻ Chủ Mưu Điều Khiển Câu Chuyện, Tôi Lật Đổ Mọi Thứ Hòng Cứu Lấy Nữ Phản Diện",
+        correctOption: "optionD"
+    },
+    {
+        question: "Bộ truyện nào sau đây đã có một thiết kế bìa độc quyền, hoàn toàn mới khi phát hành ở Việt Nam?",
+        optionA: "Tôi Yêu Nữ Phản Diện",
+        optionB: "Anh Hùng Diệt Thần & Bảy Thệ Ước",
+        optionC: "Bị Đuổi Khỏi Nhóm Anh Hùng, Tôi Muốn Sống Tự Do Tự Tại Ở Vương Đô",
+        optionD: "Bạn Gái Vs. Bạn Thời Thơ Ấu",
+        correctOption: "optionA"
+    },
+    {
+        question: "Nhân vật Nishina Toriko trong bộ tiểu thuyết “Dã Ngoại Nơi Mặt Sau của Thế Giới” đã giao cho Kamikoshi Sorawo món vũ khí nào trong lần đầu gặp mặt?",
+        optionA: "M4 CQBR",
+        optionB: "Makarov",
+        optionC: "Remington M870",
+        optionD: "AK-101",
+        correctOption: "optionB"
+    },
+    {
+        question: "Nhân vật Nagasaka Kouhei trong bộ tiểu thuyết “Ai Dám Bảo Rom-Com Không Có Ngoài Đời Thực” đã cho gương mặt của Uenohara Ayano bao nhiêu điểm?",
+        optionA: "4,3 điểm",
+        optionB: "4,7 điểm",
+        optionC: "2,8 điểm",
+        optionD: "6,9 điểm",
+        correctOption: "optionA"
+    }
+]
--- src/components/Questions/index.js
+++ src/components/Questions/index.js
@@ -1,12 +1,54 @@
+import { useState } from "react"
+
 import SectionTitle from "../SectionTitle"
 import "./Questions.scss"
+import { questionContent } from "./QuestionsContent.js"
 
 function Questions() {
+
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+
+    const quiz = questionContent.map(question => {
+        return (
+            <div>
+                <h2 className="question">{question.question}</h2>
+                <div className="answer">
+                    <div className={question.correctOption === 'optionA' ? 'optionA' : null}>A. {question.optionA}</div>
+                    <div className={question.correctOption === 'optionB' ? 'optionB' : null}>B. {question.optionB}</div>
+                    <div className={question.correctOption === 'optionC' ? 'optionC' : null}>C. {question.optionC}</div>
+                    <div className={question.correctOption === 'optionD' ? 'optionD' : null}>D. {question.optionD}</div>
+                </div>
+            </div>
+        )
+    })
+
+    const currentQuestion = quiz[currentQuestionIndex];
+
+    const nextClick = () => {
+        if (currentQuestionIndex < quiz.length - 1) {
+            setCurrentQuestionIndex(currentQuestionIndex + 1);
+        } else setCurrentQuestionIndex(0);
+    }
+
+    const prevClick = () => {
+        if (currentQuestionIndex > 0) {
+            setCurrentQuestionIndex(currentQuestionIndex - 1);
+        } else setCurrentQuestionIndex(19);
+    }
+
     return (
         <div className="wrapper">
             <SectionTitle title={"Mẫu Câu Hỏi"} description={"Đây là các câu hỏi của sự kiện năm ngoái, các mẫu câu hỏi đều sẽ tương tự như thế này!"} />
             <div className="questionContainer">
-                d
+
+                <div className="questionHeader">
+                    <button onClick={prevClick}>←</button>
+                    <h1>Câu {(currentQuestionIndex + 1) < 10 ? '0' + (currentQuestionIndex + 1) : (currentQuestionIndex + 1)}/20</h1>
+                    <button onClick={nextClick}>→</button>
+                </div>
+
+                {currentQuestion}
+
             </div>
         </div>
     );
