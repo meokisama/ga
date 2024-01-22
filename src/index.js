24497f8d4d78b600086d0a99cae1b1590ca8de28
meokisama <hoangdinhsang123@gmail.com>
2024-01-22 16:26:19 +0700

    config gooogle analytics

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import ReactGA from "react-ga4";
 
 import App from './App';
 import GlobalStyles from './components/GlobalStyles';
 import reportWebVitals from './reportWebVitals';
+
+ReactGA.initialize("G-ZLCE7RSD0S");
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,7 +17,14 @@ root.render(
   </React.StrictMode>
 );
 
+const SendAnalytics = () => {
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+  });
+}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(SendAnalytics);
