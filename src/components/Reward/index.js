087b5ce1ab2db9de10bf7ff5684e85100f17db19
meokisama <hoangdinhsang123@gmail.com>
2024-01-26 23:51:44 +0700

    finish reward

--- src/components/Reward/index.js
+++ src/components/Reward/index.js
@@ -65,60 +65,39 @@ function Reward() {
                     </div>
                 </div>
 
-                <div className='item goldBonus'>
+                {/* <div className='item goldBonus'>
                     <p>Ngoài ra, người giành Giải Nhất bên cạnh artbook còn sẽ được chúng tôi thưởng nóng thêm số tiền <span> 1.000.000đ (1 triệu đồng) </span> chuyển khoản trực tiếp!</p>
-                </div>
+                </div> */}
 
-                <div className="item silver">
-                    <div className='silverContent'>
-                        <h1>━ Giải Nhì ━</h1>
-                        <h3>Dengeki Wedding 30th Anniversary Artbook</h3>
-                        <p>Cuốn artbook kích cỡ A4 cho bộ ảnh kỉ niệm 30 năm của Dengeki Bunko. Bao gồm <strong>81 tấm ảnh váy cưới</strong> cho <strong>81 tác phẩm</strong> trong tổng số các ấn phẩm được xuất bản của họ trong 30 năm qua.</p>
-                        <div><strong>━ Thân thiện nhắc nhở ━</strong><br /> Trượt sang trái/phải ở đống ảnh để xem thêm ảnh tiếp theo!</div>
+                <div className='silverBronze'>
+                    <div className="item silver">
+                        <div className='silverContent'>
+                            <h1>━ Giải Nhì ━</h1>
+                            <h3>700.000đ</h3>
+                            <p>Sau khi cân nhắc thì chúng tôi quyết định sẽ chỉ có Giải Nhất được đồ thật vật thật.
+                                Hai giải còn lại sẽ là tiền mặt, Giải Nhì sẽ là <strong>700.000đ</strong> và chúng tôi đưa ra hai phương án nhận thưởng cho quý độc giả như sau.
+                                <br /><br /><strong>1.</strong> Nhận tiền bằng chuyển khoản trực tiếp.
+                                <br /><strong>2.</strong> Đặt hàng mua sách trực tiếp trên các nền tảng Fahasa/Shopee/Lazada/Tiki với tổng giá trị đơn hàng không vượt qua số tiền của giải thưởng.
+
+                            </p>
+                        </div>
                     </div>
-                    <div className='silverSwiper'>
-                        <Swiper
-                            effect={'cards'}
-                            grabCursor={true}
-                            modules={[EffectCards]}
-                            className="mySwiper"
-                            initialSlide={1}
-                        >
-                            <SwiperSlide>Giải nhì chưa kiếm ra</SwiperSlide>
-                            <SwiperSlide>Giải nhì chưa kiếm ra</SwiperSlide>
-                            <SwiperSlide>Giải nhì chưa kiếm ra</SwiperSlide>
-                            <SwiperSlide>Giải nhì chưa kiếm ra</SwiperSlide>
-                            <SwiperSlide>Giải nhì chưa kiếm ra</SwiperSlide>
-                        </Swiper>
-                    </div>
-                </div>
-                <div className="item bronze">
-                    <div className='bronzeSwiper'>
-                        <Swiper
-                            effect={'cards'}
-                            grabCursor={true}
-                            modules={[EffectCards]}
-                            className="mySwiper"
-                            initialSlide={1}
-                        >
-                            <SwiperSlide>Giải ba chưa kiếm ra</SwiperSlide>
-                            <SwiperSlide>Giải ba chưa kiếm ra</SwiperSlide>
-                            <SwiperSlide>Giải ba chưa kiếm ra</SwiperSlide>
-                            <SwiperSlide>Giải ba chưa kiếm ra</SwiperSlide>
-                            <SwiperSlide>Giải ba chưa kiếm ra</SwiperSlide>
-                        </Swiper>
-                    </div>
-                    <div className='bronzeContent'>
-                        <h1>━ Giải Ba ━</h1>
-                        <h3>Dengeki Wedding 30th Anniversary Artbook</h3>
-                        <p>Cuốn artbook kích cỡ A4 cho bộ ảnh kỉ niệm 30 năm của Dengeki Bunko. Bao gồm <strong>81 tấm ảnh váy cưới</strong> cho <strong>81 tác phẩm</strong> trong tổng số các ấn phẩm được xuất bản của họ trong 30 năm qua.</p>
-                        <div><strong>━ Thân thiện nhắc nhở ━</strong><br /> Trượt sang trái/phải ở đống ảnh để xem thêm ảnh tiếp theo!</div>
+                    <div className="item bronze">
+                        <div className='bronzeContent'>
+                            <h1>━ Giải Ba ━</h1>
+                            <h3>500.000đ</h3>
+                            <p>
+                                Tương tự Giải Nhì thì Giải Ba cũng sẽ là tiền mặt với số tiền là <strong>500.000đ</strong> và cũng với 2 lựa chọn nhận thưởng như vậy.
+                                Tất nhiên nếu chọn phương án mua hàng thì chúng tôi khuyến khích là đơn hàng liên quan tới tiểu thuyết hoặc truyện tranh.
+                                <br />Một mục đích khác của cuộc thi này với những câu hỏi trắc nghiệm và phần thưởng giá trị như vậy chính là để cổ vũ hỗ trợ phát triển văn hóa đọc nói chung đặc biệt là light novel nói riêng.
+                            </p>
+                        </div>
                     </div>
                 </div>
                 <img id="alisa" alt="Alisa" src={alisa} />
                 <img id="liar" alt="" src={liar} />
-            </div>
-        </div>
+            </div >
+        </div >
     );
 }
 
