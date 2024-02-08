63a363f4584cc2c9ba37d0c99df609336a435d1d
meokisama <hoangdinhsang123@gmail.com>
2024-02-08 21:12:38 +0700

    last update

--- src/components/KonoRano/index.js
+++ src/components/KonoRano/index.js
@@ -1,3 +1,6 @@
+import { DownloadOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
+
 import "./KonoRano.scss"
 import SectionTitle from "../SectionTitle"
 import kono2021 from "../../assets/konorano/2021.webp"
@@ -12,24 +15,28 @@ function KonoRano() {
             cover: kono2024,
             year: "2024",
             url: "https://url.meoki.vn/konorano2024epub",
+            vi: "https://docs.google.com/spreadsheets/d/1VbVL3bIBZIMJ9u8fQL73-kGTt2WjxAkBZw6wgw6_oQc/edit?usp=sharing",
             border: { borderBottom: "7px solid #f9ac02" }
         },
         {
             cover: kono2023,
             year: "2023",
             url: "https://url.meoki.vn/konorano2023",
+            vi: "https://docs.google.com/spreadsheets/d/1_tL8XRLyMAq3VvPOxhIkl5L7n3HbGR2iow9uJDyMlqs/edit?usp=sharing",
             border: { borderBottom: "7px solid #e62229" }
         },
         {
             cover: kono2022,
             year: "2022",
             url: "https://url.meoki.vn/konorano2022",
+            vi: "https://docs.google.com/spreadsheets/d/10b0cGsEBOeHWUVdZl6Hepzekl_3cc48pT4aoolkWUZE/edit?usp=sharing",
             border: { borderBottom: "7px solid #fc4d06" }
         },
         {
             cover: kono2021,
             year: "2021",
             url: "https://url.meoki.vn/konorano2021",
+            vi: "https://docs.google.com/spreadsheets/d/1fYgDR2fc_zgmvsmGV1zgGqmxKHIlL7_WoQgCUMbDxv4/edit?usp=sharing",
             border: { borderBottom: "7px solid #01a5e9" }
         }
     ]
@@ -38,19 +45,25 @@ function KonoRano() {
         <div className="wrapper konorano">
             <SectionTitle
                 title="Tài Liệu"
-                description="Chia sẻ tài liệu ôn bài. Kono Light Novel ga Sugoi cho 4 năm gần nhất! (EPUB)"
+                description="Chia sẻ tài liệu ôn bài. Kono Light Novel ga Sugoi cho 4 năm gần nhất! Cả bản gốc tiếng Nhật (EPUB), và bản Việt từ @NaviRanobe (Sheet)."
                 titleType="type2"
             />
             <div className="gridKono">
                 {items.map((item, index) => {
                     return (
-                        <div key={index} className="gridItem" style={item.border} onClick={() => window.open(item.url, "_blank")}>
+                        <div key={index} className="gridItem" style={item.border}>
                             <img alt="" src={item.cover} />
                             <div className="info">
                                 <h2>Kono Light Novel ga Sugoi! {item.year}</h2>
-                                <p>
-                                    Nhấn vào khung này để dẫn link tải xuống cho bản gốc tiếng Nhật <strong>KonoRano {item.year}</strong> với định dạng sách điện tử EPUB!
-                                </p>
+                                <p>Nhấn vào nút tương ứng cho bản Nhật hoặc Việt để truy cập nội dung.</p>
+                                <div className='downloadBtn'>
+                                    <Button type="primary" icon={<DownloadOutlined />} size="large" onClick={() => window.open(item.url, "_blank")}>
+                                        Bản gốc tiếng Nhật (EPUB)
+                                    </Button>
+                                    <Button type="primary" icon={<DownloadOutlined />} size="large" onClick={() => window.open(item.vi, "_blank")}>
+                                        Bản dịch tiếng Việt (Sheet)
+                                    </Button>
+                                </div>
                             </div>
                         </div>
                     )
